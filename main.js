6007af1d30c62d0b814619dbadedaea1d79023ad
João Felipi Cardoso <joaofelipi877@gmail.com>
2023-08-22 09:04:50 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import express from "express"
-const port = 9204;
+const port = 9205;
 const app = express();
 
 app.get("/", (req, res) => {
 	console.log(`GET from ${req.ip}`)
-	res.send("Olá mundo, essa é a aplicação node")
+	res.send("Olá mundo, essa é a aplicação node, teste")
 });
 
 app.listen(port, () => {
